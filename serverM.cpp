a62ce0aa4865f3e1733956a5e8f81eef550e3a5a
OfferPlease <zhouyuxin0106@gmail.com>
2023-11-15 15:42:41 -0800

    server basic

---
+++ serverM.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstring>
+#include <unistd.h>
+
+int main() {
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    const char *message = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Setup some settings
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(45367);
+
+    // Forcefully bind socket to the port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    // listen to the port
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
+        {
+        perror("accept");
+        exit(EXIT_FAILURE);
+        }
+        read(new_socket, buffer, 1024);
+        std::cout << buffer << std::endl;
+        send(new_socket, message, strlen(message), 0);
+        std::cout << "Hello message sent\n";
+    }
+    
+    return 0;
+}
